025d45ac2fccae39fbfccd8722c6cc7682a8ded6
lupitadavila <lupita.dav@gmail.com>
2023-09-29 15:43:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Graphql-Example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-dijrkz)
---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+class DownstreamError extends Error {
+  constructor(message, errorCode) {
+    super(message);
+    this.name = 'DownstreamError';
+    this.code = errorCode;
+  }
+}
+
+// API
+const BASE_URI = 'https://jsonplaceholder.typicode.com/posts';
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    getPosts: [Post]
+    getPostById(id: ID!): GetPostResponse
+  }
+  type Mutation {
+    createPost(title: String!, body: String!, userId: ID!): CreatePostResponse
+  }
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    userId: ID!
+  }
+  type PostError {
+    message: String
+    code: String
+  }
+
+  union CreatePostResponse = Post | PostError
+  union GetPostResponse = Post | PostError
+`);
+
+const getPosts = async () => {
+  return await fetch(BASE_URI).then((response) => response.json());
+};
+
+const getPostById = async ({ id }) => {
+  try {
+    const post = await fetch(`${BASE_URI}/${id}`).then((response) => {
+      if (!response.ok) {
+        throw new DownstreamError('API error', response.status);
+      }
+      return response.json();
+    });
+    return {
+      __typename: 'Post',
+      ...post,
+    };
+  } catch (e) {
+    if (e instanceof DownstreamError) {
+      return {
+        __typename: 'PostError',
+        message: 'Post API responded with error',
+        code: e.code,
+      };
+    }
+    return {
+      __typename: 'PostError',
+      message: e.message,
+    };
+  }
+};
+
+const createPost = async (input) => {
+  const post = await fetch(BASE_URI, {
+    method: 'POST',
+    body: JSON.stringify(input),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json());
+
+  if (!post) {
+    return {
+      __typename: 'PostError',
+      message: 'Post could not be created',
+      code: '1000',
+    };
+  }
+  return {
+    __typename: 'Post',
+    ...post,
+  };
+};
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  getPosts,
+  getPostById,
+  createPost,
+};
+
+const app = express();
+
+app.use(
+  '/',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () =>
+  console.log('Running a GraphQL API server at http://localhost:4000/')
+);
